05a5acc40e928d016ab60f858ad3e21e555b465f
benjguin <benjamin@3-4.fr>
2016-11-30 14:07:30 +0100

    wip

--- code/buildimages.sh
+++ code/buildimages.sh
@@ -19,10 +19,15 @@ fi
 build_and_push()
 {
     folderpath=$1
-    filepath=$1/Dockerfile
+    filepath=$folderpath/Dockerfile
+    filepath2=$folderpath/tmpDockerfile
     tagname=$(eval echo "`head -1 $filepath | awk '{print $2}'`")
     tagversion=`head -3 $filepath | tail -1| awk '{print $3}'`
     fulltag="$tagname:$tagversion"
+
+    if test -e $filepath2; then rm $filepath2; fi
+    replacestring="s/\$BOONTADATA_DOCKER_REGISTRY/${BOONTADATA_DOCKER_REGISTRY}/g"
+    sed $replacestring $filepath > $filepath2
 
     if test $reset = "reset"
     then
@@ -30,14 +35,16 @@ build_and_push()
         docker rmi $fulltag
     fi
 
-    docker build -t $fulltag $folderpath
+    docker build -t $fulltag $folderpath --file tmpDockerfile
     docker push $fulltag
 }
 
+build_and_push $BOONTADATA_HOME/code/pyclientbase
+build_and_push $BOONTADATA_HOME/code/pyclient
 build_and_push $BOONTADATA_HOME/code/cassandrainit
-build_and_push $BOONTADATA_HOME/code/devscala
-build_and_push 
-build_and_push pyclientbase ./pyclientbase
-build_and_push kafkaserver ./kafka-docker
-build_and_push flink ./flink/base
-
+build_and_push $BOONTADATA_HOME/code/devjvm
+build_and_push $BOONTADATA_HOME/code/flink/base
+build_and_push $BOONTADATA_HOME/code/flink/master
+build_and_push $BOONTADATA_HOME/code/flink/worker
+build_and_push $BOONTADATA_HOME/code/kafka-docker
+build_and_push $BOONTADATA_HOME/code/spark
