2dd45afeeb0c17268b9723058d5c908573d01dc4
benjguin <benjamin@3-4.fr>
2016-12-19 15:48:11 +0000

    wip

--- code/spark/test.py
+++ code/spark/test.py
@@ -1,4 +1,13 @@
-from pyspark.sql import SQLContext
+#import pip
+
+#try:
+#    from pyspark_cassandra import streaming
+#except ImportError:
+#    pip.main(["install", "cassandra-driver"])
+#    from pyspark_cassandra import streaming
+
+#from pyspark_cassandra import streaming
+
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
@@ -22,18 +31,35 @@ def main():
 
     # set up our contexts
     sc = SparkContext(conf=conf) 
-    sql = SQLContext(sc)
     streamingContext = StreamingContext(sc, batchDuration=5)
 
     kafka_stream = KafkaUtils.createDirectStream(streamingContext,
         ["sampletopic"], 
         {"metadata.broker.list": "ks1:9092,ks2:9092,ks3:9092"})
 
-    parsed = kafka_stream.map(lambda event: parseEvent(event[1]))
+    parsed = kafka_stream \
+        .map(lambda event: parseEvent(event[1]))
 
-    parsed.pprint()
-    # sc.parallelize(aggregated) \
-    #    .saveToCassandra("boontadata", "agg_events")
+    # remove duplicates
+    parsedDeduplicated = parsed.map(lambda event: (event[FIELD_MESSAGE_ID],event)) \
+        .reduceByKey(lambda x,y: y)
+
+    aggregated = parsedDeduplicated.map(lambda event:
+        (
+            (event[1][FIELD_DEVICE_ID], event[1][FIELD_CATEGORY]),
+            (int(event[1][FIELD_MEASURE1]), float(event[1][FIELD_MEASURE2]))
+        )) \
+        .reduceByKey(lambda vN,vNplus1: (vN[0] + vNplus1[0], vN[1] + vNplus1[1])) \
+        .transform(lambda time,rdd: rdd \
+            .map(lambda kv: {
+                "window_time": time,
+                "device_id": kv[0][0],
+                "category": kv[0][1], 
+                "m1_sum_spark": kv[1][0],
+                "m2_sum_spark": kv[1][1] }))
+
+    aggregated.pprint()
+    #aggregated.saveToCassandra("boontadata", "agg_events")
 
     streamingContext.start()
     streamingContext.awaitTermination()
