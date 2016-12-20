4e97597cf41c89f3f3ea41de412aef2f9e05faf6
benjguin <benjamin@3-4.fr>
2016-12-20 11:14:29 +0000

    wip

--- code/spark/test.py
+++ code/spark/test.py
@@ -17,37 +17,44 @@ FIELD_CATEGORY = 3
 FIELD_MEASURE1 = 4
 FIELD_MEASURE2 = 5
 
+csession = None
+ccluster = None
+
 def parseEvent(message):
     return message.split('|')
 
 # NB: could not make the standard saveToCassandra method work in this context where Spark and Cassandra are in different clusters
 #please, contribute if you know how to enhance this
-def saveRowToCassandra(csession, e):
+def transformAndSaveRowToCassandra(time, kv):
+    r= {
+        "window_time": time,
+        "device_id": kv[0][0],
+        "category": kv[0][1], 
+        "m1_sum_spark": kv[1][0],
+        "m2_sum_spark": kv[1][1] }
+
+    if csession is None:
+        ccluster=Cluster(['cassandra1', 'cassandra2', 'cassandra3'])
+        csession=ccluster.connect('boontadata')
+
     csession.execute("INSERT INTO agg_events "
         + "(window_time, device_id, category, m1_sum_spark, m2_sum_spark) " \
         + "VALUES ('{0}', '{1}', '{2}', {3}, {4})"
         .format(
-            str(e['window_time']), 
-            str(e['device_id']), 
-            str(e['category']),
-            int(e['m1_sum_spark']),
-            float(e['m2_sum_spark']))
+            str(r['window_time']), 
+            str(r['device_id']), 
+            str(r['category']),
+            int(r['m1_sum_spark']),
+            float(r['m2_sum_spark'])))
+    return r
 
 def main():
-    #connect to Cassandra
-    ccluster=Cluster(['cassandra1', 'cassandra2', 'cassandra3'])
-    csession=ccluster.connect('boontadata')
-
     #connect to Spark
     conf = SparkConf() \
         .setAppName("boontadata-streams-spark") \
         .setMaster("spark://sparkm1:7077")
     sc = SparkContext(conf=conf) 
     streamingContext = StreamingContext(sc, batchDuration=5)
-
-    sc.parallelize([{"id":"testing1", "message": "from Spark 1"},
-        {"id":"testing2", "message": "from Spark 2"}]) \
-	.saveToCassandra("boontadata", "debug", {"id", "message"})
 
     kafka_stream = KafkaUtils.createDirectStream(streamingContext,
         ["sampletopic"], 
@@ -67,24 +74,17 @@ def main():
         )) \
         .reduceByKey(lambda vN,vNplus1: (vN[0] + vNplus1[0], vN[1] + vNplus1[1])) \
         .transform(lambda time,rdd: rdd \
-            .map(lambda kv: {
-                "window_time": time,
-                "device_id": kv[0][0],
-                "category": kv[0][1], 
-                "m1_sum_spark": kv[1][0],
-                "m2_sum_spark": kv[1][1] }))
+            .map(lambda kv: transformAndSaveRowToCassandra(time, kv)))
 
     aggregated.pprint()
-
-    # save to Cassandra
-    aggregated.map(lambda e: saveRowToCassandra(csession, e))
 
     #could not have the correct jars, or the correct architecture to make the following line working:
     #aggregated.saveToCassandra("boontadata", "agg_events")
 
     streamingContext.start()
     streamingContext.awaitTermination()
-    ccluster.shutdown()
+    if ccluster is not None:
+        ccluster.shutdown()
     print("OK.")
 
 if __name__ == '__main__':
