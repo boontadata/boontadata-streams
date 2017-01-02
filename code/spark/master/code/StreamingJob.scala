ae09511512626b2fedcd4e42ac337904e8983227
benjguin <benjamin@3-4.fr>
2017-01-02 18:45:19 +0100

    print aggregates in Scala

--- code/spark/master/code/StreamingJob.scala
+++ code/spark/master/code/StreamingJob.scala
@@ -17,10 +17,30 @@ class IotEvent(
   }
 }
 
-object IotEvent extends Serializable {
+object IotEventFactory {
   def fromString(in: String): IotEvent = {
     val parts = in.split("\\|")
     new IotEvent(parts(0), parts(1), parts(2), parts(3), parts(4).toInt, parts(5).toFloat)
+  }
+  def fromParts(
+    messageId: String,
+    deviceId: String,
+    timestamp: String,
+    category: String, 
+    measure1: Int, 
+    measure2: Float): IotEvent = {
+    new IotEvent(messageId, deviceId, timestamp, category, measure1, measure2)
+  }
+}
+
+class Aggregate(
+  val window_time: Long,
+  val device_id: String,
+  val category: String, 
+  val m1_sum_spark: Int, 
+  val m2_sum_spark: Float) extends Serializable {
+  override def toString(): String = {
+    "%s\t%s\t%s\t%s\t%s\n".format(window_time, device_id, category, m1_sum_spark, m2_sum_spark)
   }
 }
 
@@ -50,47 +70,24 @@ object DirectKafkaAggregateEvents {
 
     val lines = messages.map(tuple => tuple._2) // Spark receives Kafka payload as the second field of the tuple
     
-    val parsed = lines.map(IotEvent.fromString(_))
+    val parsed = lines.map(IotEventFactory.fromString(_))
     
     val parsedDeduplicated = parsed
-      .map(event => (event.messageId, event))
+      .map(e => (e.messageId, e))
       .reduceByKey((vN, vNplus1) => vNplus1)
-      .map({ case (k: String, event: IotEvent) => event})
-
-    val aggregated = parsedDeduplicated
-      .map({ case event: IotEvent => ((event.deviceId, event.category), event)})
-      .reduceByKey({ case (vN: IotEvent, vNplus1: IotEvent) 
-        => 
-        val deviceId = vN.deviceId
-        val category = vN.category 
-        val timestamp = vN.timestamp
-        val sumM1 = vN.measure1 + vN.measure1
-        val sumM2 = vN.measure2 + vN.measure2
-        })
       .map(tuple => tuple._2)
 
-    /*
-   
-    val aggregated = parsedDeduplicated.map(event =>
-      (
-        (event._2[FIELD_DEVICE_ID], event._2[FIELD_CATEGORY]),
-        (int(event._2[FIELD_MEASURE1]), float(event._2[FIELD_MEASURE2]))
-      ))
-        .reduceByKey(vN,vNplus1 => (vN._1 + vNplus1._1, vN._2 + vNplus1._2))
-        .transform(time,x => x
-          .map(kv => new {
-            val window_time = time
-            val device_id = kv._1._1
-            val category = kv._1._2 
-            val m1_sum_spark = kv._2._1
-            val m2_sum_spark = kv._2._2 }))
-    */
+    val aggregated = parsedDeduplicated
+      .map(e => ((e.deviceId, e.category), e))
+      .reduceByKey(
+        (vN, vNplus1)
+        => 
+        IotEventFactory.fromParts("", vN.deviceId, "", vN.category, vN.measure1 + vN.measure1, vN.measure2 + vN.measure2))
+      .transform((rdd, time) => rdd
+        .map({ case(k, e) => new Aggregate(
+          time.milliseconds, e.deviceId, e.category, e.measure1, e.measure2)}))
 
     aggregated.print()
-
-    /*
-
-    */
 
     //parsedDeduplicated.print()
     //parsed.foreachRDD(_.collect().foreach(println))
